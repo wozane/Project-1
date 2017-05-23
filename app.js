1c85a19b2b17ddae396e3d4c7260e87d7df54bfd
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-05-23 16:28:09 +0200

    Load navbar menu on mobile view with app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.getElementById("main-nav").addEventListener("click", function (event) {
+    document.querySelector("body").classList.toggle("menu-mobile-active");
+  }, false);
+
+  document.getElementById("team-show-more").addEventListener("click", function (event) {
+    
+  }, false);
+});
