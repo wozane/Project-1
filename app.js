facaaf40e4cca6e630b21da906cb8e6468a2890f
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-05-29 08:34:43 +0200

    Further refactor app.js file and and 'only screen' to @media

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.getElementById("main-nav").addEventListener("click", function (event) {
-    document.querySelector("body").classList.toggle("menu-mobile-active");
-  });
+// toggle Menu on mobile devices
 
-  document.getElementById("team-show-more").addEventListener("click", function (event) {
-    event.preventDefault();
-    var teamSection = document.getElementById("team");
-    teamSection.classList.toggle("expand-team-members");
+var toggleMenu =  function(event) {
+  document.querySelector("body").classList.toggle("menu-mobile-active");
+};
 
-    if(!teamSection.classList.contains("expand-team-members")) {
-      window.scrollTo(0, teamSection.offsetTop);
-    };
-  });
+document.getElementById("main-nav").addEventListener("click", toggleMenu);
 
-  document.getElementById("main-nav").addEventListener()
-});
+
+//toggle TeamSection
+
+var toggleTeam = function(event) {
+  event.preventDefault();
+  var teamSection = document.getElementById("team");
+  teamSection.classList.toggle("expand-team-members");
+
+  if(!teamSection.classList.contains("expand-team-members")) {
+    window.scrollTo(0, teamSection.offsetTop);
+  };
+};
+
+document.getElementById("team-show-more").addEventListener("click", toggleTeam);
